33b7e43ebb2348ac9aab7f305c23d567d47f3f8d
Kelvin Charles <kelvinc1291@hotmail.com>
2023-02-11 13:36:29 -0300

    Primeiro commit, projeto concluido com todos arquivos

---
+++ README.md
@@ -0,0 +1,40 @@
+# 🖥️ Projeto livre da comunidade Desbravando a Programação
+
+## Descrição
+
+Projeto realizado com colaboração, afim de treinar trabalho em equipe e trocar conhecimentos e experiências !
+
+## ✍️ Processo
+
+    - Marcação semântica HTML5
+    - Propriedades personalizadas de CSS3
+    - Flexbox
+    - Design responsivo
+    - JavaScript
+    - APIs
+
+🖱️ A página
+<img src="src/design/projeto-pokemon.gif" alt="Gif exibindo o desktop e versão mobile do site">
+
+👩‍💻 Dev's
+
+<table align="center">
+    <tr>
+        <td align="center">
+            <div>
+                <img src="https://avatars.githubusercontent.com/u/110488969?v=4" width="120px;" alt="Foto de Kelvin no GitHub"/><br>
+                <b> Kelvin Charles </b><br>
+                <a href="https://www.linkedin.com/in/kelvin-charles/" alt="Linkedin"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" height="20"></a>
+                <a href="https://github.com/kelvincharlesdev" alt="GitHub"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" height="20"></a>
+            </div>
+        </td>
+        <td align="center">
+            <div>
+                <img src="https://avatars.githubusercontent.com/u/85200035?v=4" width="120px;" alt="Foto de Jean no GitHub"/><br>
+                <b> Jean Leão </b><br>
+                <a href="https://www.linkedin.com/in/jean-leao/" alt="Linkedin"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" height="20"></a>
+                <a href="https://github.com/JeanLeao" alt="GitHub"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" height="20"></a>
+            </div>
+        </td>
+    </tr>
+</table>
---
+++ src/js/index.js
@@ -0,0 +1,215 @@
+var corpo = document.getElementById("container");
+var colors = [
+  "#7ED876",
+  "#63DBF3",
+  "#E2842E",
+  "#FFD506",
+  "#663399",
+  "#676768",
+  "#DCC200",
+  "#796BAE",
+];
+var type = [
+  "grass",
+  "water",
+  "fire",
+  "electric",
+  "normal",
+  "bug",
+  "ground",
+  "poison",
+];
+
+renderizar()
+
+function renderizar (){
+
+axios.get("https://pokeapi.co/api/v2/pokemon?limit=27&offset=0").then((data) => {
+    document.getElementById("error").style.display = "none"
+    
+    var lista = data.data.results;
+
+    lista.forEach((pokemon) => {
+        axios.get(pokemon.url).then((url) => {
+            var div = document.createElement("div");
+            div.innerHTML = ` 
+                    <div class="card">
+
+                        <div class="info ${pokemon.name}">  
+
+                            <div class="img">
+                                <img src="${
+                                  url.data.sprites.other.dream_world
+                                    .front_default
+                                }">
+                            </div>
+                                
+                            <h2>${pokemon.name}</h2>
+                            <p>${url.data.types[0].type.name}</p>
+
+                        </div> 
+
+                        <div class="stats">
+                            <ul>
+                                <li>
+                                    <div class="stats-name">
+                                        <p>Hp</p>
+                                        
+                                        
+                                        
+                                        <div class="barra-cheia">
+                                            <div class="barra-porcentagem"
+                                                style="width:${url.data.stats[0].base_stat}%;
+                                                background-color: ${colors[type.indexOf(url.data.types[0].type.name)]};">
+                                            </div> 
+                                        </div> 
+                                    </div>
+                                </li>    
+
+                                <li>
+                                    <div class="stats-name">
+                                        <p>Speed</p>
+                                        
+                                        
+                                        <div class="barra-cheia">
+                                            <div class="barra-porcentagem" 
+                                                style="width:${url.data.stats[5].base_stat}%;
+                                                background-color: ${colors[type.indexOf(url.data.types[0].type.name)]}">
+                                            </div> 
+                                        </div> 
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="stats-name">
+                                        <p>Attack</p>
+                                        
+                                        
+                                        <div class="barra-cheia">
+                                            <div class="barra-porcentagem" 
+                                                style="width:${url.data.stats[1].base_stat}%;
+                                                background-color: ${colors[type.indexOf(url.data.types[0].type.name)]}">
+                                            </div> 
+                                        </div> 
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div class="stats-name">
+                                        <p>Defense</p>
+                                        
+                                        
+                                        <div class="barra-cheia">
+                                            <div class="barra-porcentagem" 
+                                                style="width:${url.data.stats[2].base_stat}%; 
+                                                background-color: ${colors[type.indexOf(url.data.types[0].type.name)]}">
+                                            </div> 
+                                        </div> 
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    
+                    </div>
+
+                    
+
+                `                  
+        corpo.appendChild(div);
+      });
+    });
+  });
+}
+
+function buscar() {
+  axios.get("https://pokeapi.co/api/v2/pokemon/" + document.getElementById("pokemon").value).then((data) => {
+    document.getElementById("error").style.display = "none"
+      corpo.innerHTML = ` 
+        <div class="card unico">
+
+            <div class="info ">  
+
+                <div class="img">
+                    <img src=" ${data.data.sprites.other.dream_world.front_default}">
+                </div>
+                    
+            
+
+            </div> 
+
+            <div class="stats">
+                <h2>${data.data.name}</h2>
+                <p>${data.data.types[0].type.name}</p>
+                <ul>
+                    <li>
+                        <div class="stats-name">
+                            <p>Hp</p>
+                            
+                            
+                            <div class="barra-cheia">
+                                <div class="barra-porcentagem" 
+                                    style="width: ${data.data.stats[0].base_stat}%; 
+                                    background-color:${colors[type.indexOf(data.data.types[0].type.name)]};">
+                                </div> 
+                            </div> 
+                        </div>
+                    </li>    
+
+                    <li>
+                        <div class="stats-name">
+                            <p>Speed</p>
+                            
+                            
+                            <div class="barra-cheia">
+                                <div class="barra-porcentagem" 
+                                    style="width: ${data.data.stats[5].base_stat}%; 
+                                    background-color:${colors[type.indexOf(data.data.types[0].type.name)]};">
+                                </div> 
+                            </div> 
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="stats-name">
+                            <p>Attack</p>
+                            
+                            
+                            <div class="barra-cheia">
+                                <div class="barra-porcentagem" 
+                                    style="width: ${data.data.stats[1].base_stat}%; 
+                                    background-color:${colors[type.indexOf(data.data.types[0].type.name)]};">
+                                </div> 
+                            </div> 
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="stats-name">
+                            <p>Defense</p>
+                    
+                            <div class="barra-cheia">
+                                <div class="barra-porcentagem" 
+                                    style="width:${data.data.stats[2].base_stat}%; 
+                                    background-color:${colors[type.indexOf(data.data.types[0].type.name)]};">
+                                </div> 
+                            </div> 
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+    `;
+    }).catch((error) =>{
+        document.getElementById("error").style.display = "block"
+    })
+}
+
+function voltar(){
+    document.getElementById("error").style.display = "none"
+    corpo.innerHTML = "" 
+    document.getElementById("pokemon").value = ""
+
+    renderizar()
+
+}
